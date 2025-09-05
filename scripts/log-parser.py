c66ca41a31916be9d392d9596c047a88cd7c9ef2
Ida A. <asplacida08@gmail.com>
2025-09-05 05:05:09 +0200

    Update log-parser.py

--- scripts/log-parser.py
+++ scripts/log-parser.py
@@ -1,1 +1,10 @@
+"""
+Ce script Python est conçu pour analyser les logs produits par Cowrie.
+Il commence par parcourir les journaux au format JSON afin d’en extraire les informations essentielles.
+À partir de ces données, il génère différentes statistiques, comme le nombre de tentatives d’authentification,
+les connexions réussies ou encore les commandes exécutées. Il met aussi en évidence certaines tendances,
+par exemple les adresses IP les plus actives ainsi que les identifiants et mots de passe les plus testés
+par les attaquants. Enfin, les résultats sont présentés directement dans le terminal, offrant une vue d’ensemble
+rapide et claire de l’activité observée.
+"""
 
